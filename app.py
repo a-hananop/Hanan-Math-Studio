57fdeab30f1edfc4e96d3f8ca2f2f8bbd28d46f6
a-hananop <ahglobal251@gmail.com>
2026-04-23 02:18:55 +0500

    Change app.run to use host '0.0.0.0'

--- app.py
+++ app.py
@@ -2161,4 +2161,4 @@ if __name__ == '__main__':
         print("⚠ Prolog not available - Install PySwip for full features")
     print("="*80 + "\n")
     
-    app.run(debug=True, port=5000, use_reloader=False, threaded=False)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False, threaded=False)
